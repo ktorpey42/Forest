3e4e805384995a0a851a167966f5aaeaa3714bb8
ktorpey42 <ktorpey42@gmail.com>
2020-07-25 15:09:37 +0100

    Revert to original google code

--- js/big.js
+++ js/big.js
@@ -1,50 +1,87 @@
+
+
+
+
+
+
+
+
+// Initialize and add the map
+//function myMap() {
+  // The location of Uluru
+  //var uluru = {lat: 53.312642, lng: -6.281301};
+  // The map, centered at Uluru
+//var map = new google.maps.Map(
+    //  document.getElementById('googleMap'), {zoom: 13, center: uluru});
+  // The marker, positioned at Uluru
+//  var marker = new google.maps.Marker({position: uluru, map: map});
+//}
+
 // Add new map code with HTML 5 Geolocation
 
-var map;
-function initialize() {
-  var center = new google.maps.LatLng(-34.397, 150.644);
+var map, infoWindow;
+function initMap() {
   map = new google.maps.Map(document.getElementById('map'), {
-    center: center,
+    center: {lat: -34.397, lng: 150.644},
     zoom: 13
   });
+  infoWindow = new google.maps.InfoWindow;
 
-  var request = {
-    location: center,
-    radius: 8047,
-    types: ['cafe']
+  // Try HTML5 geolocation.
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+      var pos = {
+        lat: position.coords.latitude,
+        lng: position.coords.longitude
+      };
 
-  };
+      infoWindow.setPosition(pos);
+      infoWindow.setContent('Location found.');
+      infoWindow.open(map);
+      map.setCenter(pos);
+    }, function() {
+      handleLocationError(true, infoWindow, map.getCenter());
+    });
+  } else {
+    // Browser doesn't support Geolocation
+    handleLocationError(false, infoWindow, map.getCenter());
+  }
+}
 
-  var service = new google.maps.places.PlacesService(map);
-
-  service.nearbySearch(request, callback);
-
-
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+  infoWindow.setPosition(pos);
+  infoWindow.setContent(browserHasGeolocation ?
+                        'Error: The Geolocation service failed.' :
+                        'Error: Your browser doesn\'t support geolocation.');
+  infoWindow.open(map);
 }
 
 
 
-function callback(results, status) {
-    if(status == google.maps.places.PlacesServiceStatus.OK) {
-        for (var i = 0; i < results.length; i++) {
-          createMarker(results[i]);
-        }
-    }
+// Additional code for showing places on Google maps
+
+
+
+
+
+function myFunction() {
+  var x = document.getElementById("myTopnav");
+  if (x.className === "topnav") {
+    x.className += " responsive";
+  } else {
+    x.className = "topnav";
+  }
 }
-
-function createMarker(place) {
-      var placeLoc = place.geometry.location;
-      var marker = new google.maps.Marker({
-        map: map,
-        position: place.geometry.location
-      });
-}
-
-google.maps.event.addDomListener(window, 'load', initialize);
-
-// Other coding
-
 
 
 var dt = new Date();
 document.getElementById("datetime").innerHTML = dt.toLocaleTimeString();
+
+
+function showInput() {
+    document.getElementById('display').innerHTML =
+                document.getElementById("user_input").value;
+    }
+
+
+  var x = document.getElementById("demo");
