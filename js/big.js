bdae4f2ee6dc033c79b58a9d1c58141b151dbce9
ktorpey42 <ktorpey42@gmail.com>
2020-07-26 19:42:55 +0100

    Change zoom level

--- js/big.js
+++ js/big.js
@@ -1,19 +1,3 @@
-// Add new map code with HTML 5 Geolocation
-
-
-
-
-
-
-//google.maps.event.addDomListener(window, 'load', initMap);
-// Additional code for showing places on Google maps
-
-
-
-
-
-
-
 
 var dt = new Date();
 document.getElementById("datetime").innerHTML = dt.toLocaleTimeString();
